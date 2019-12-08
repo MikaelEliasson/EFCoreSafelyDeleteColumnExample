77afcd9a80c575eb637b9fa202cf5ccdd81f4e54
Mikael Eliasson <mikael@bokio.se>
2019-12-08 17:56:38 +0100

    Step #2: Remove the column from the database
    Simply runs the migration EF wanted to apply in Step #1. But after Step #1  has been running on all instances for a while.

---
+++ EFCoreSafelyDeleteColumnExample/EFCoreSafelyDeleteColumnExample/Migrations/20191208165312_RemoveColumnFromDatabase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCoreSafelyDeleteColumnExample.Migrations
+{
+    public partial class RemoveColumnFromDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            /* Now we remove this column. This would be done some time after the previous migration has been running in prod for a while.
+             * We can rollback to the previous version of the code and run on this DB schema version. But we could not run the "Initial"
+             * version of the code on this schema
+             */
+
+            migrationBuilder.DropColumn(
+                name: "PropToBeDeleted",
+                table: "Persons");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PropToBeDeleted",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
